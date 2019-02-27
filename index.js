bef592ae21242c0a16dbcb421e775465e8231d18
Dadi Ilham Setiadi <46667343+idadhamz@users.noreply.github.com>
2019-02-27 20:21:22 +0700

    update terbaru

--- index.js
+++ index.js
@@ -1,48 +1,54 @@
 $('body').ready(function(){
+
     var angka_1;
     var angka_2;
-    var operator;
-    var ket_operator = $('#ket_operator');
     var hasil = $("#result");
+    
+    // var operator;
+    // var ket_operator = $('#ket_operator');
 
-    $(".field .operators a").click(function(){
-        operator = $(this).text();
-        switch(operator){
-            case '+':
-                ket_operator.val('Penjumlahan')
-                break;
-            case '-':
-                ket_operator.val('Pengurangan')
-                break;
-            case '/':
-                ket_operator.val('Pembagian')
-                break;
-            case 'x':
-                ket_operator.val('Perkalian')
-                break;
-        }
-    });
+    // $(".field .operators a").click(function(){
+    //     operator = $(this).text();
+    //     switch(operator){
+    //         case '+':
+    //             ket_operator.val('Penjumlahan')
+    //             break;
+    //         case '-':
+    //             ket_operator.val('Pengurangan')
+    //             break;
+    //         case '/':
+    //             ket_operator.val('Pembagian')
+    //             break;
+    //         case 'x':
+    //             ket_operator.val('Perkalian')
+    //             break;
+    //     }
+    // });
 
     $("#jumlah").click(function(){
-        var total;
 
         angka_1 = $('#first').val();
-        angka_2 = $('#second').val(); 
+        angka_2 = $('#second').val();
+
+        var total;
+        var operator = document.getElementById("operator");
+        var operator_value = operator.options[operator.selectedIndex].value;
 
         console.log(angka_1);
         console.log(angka_2);
+        console.log(operator_value);
 
-        switch(operator){
-            case '+':
+        switch(operator_value){
+            case 'tambah':
                 total = parseFloat(angka_1) + parseFloat(angka_2);
                 break;
-            case '-':
+            case 'kurang':
                 total = parseFloat(angka_1) - parseFloat(angka_2);
                 break;
-            case '/':
+            case 'bagi':
                 total = parseFloat(angka_1) / parseFloat(angka_2);
                 break;
-            case 'x':
+            case 'kali':
                 total = parseFloat(angka_1) * parseFloat(angka_2);
                 break;
             default:
@@ -58,7 +64,7 @@ $('body').ready(function(){
         angka_1 = $('#first').val(null);
         angka_2 = $('#second').val(null);
         hasil.val(null);
-        ket_operator.val(null);
+        // ket_operator.val(null);
     }  
 
     $("#ulang").click(function(){
